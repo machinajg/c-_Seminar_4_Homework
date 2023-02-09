729c573096fcafdb0071c4f95b5f96b5a57e0209
Julia Machina <machinajg@gmail.com>
2023-02-09 16:23:16 +0300

    Pow of number project added

---
+++ pow_number/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+// 1). Цикл for (без метода)
+
+// Console.Write("Введите число А:  ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B:  ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int res = 1;
+// for (int i = 0; i < B; i ++ )
+// {
+//     res = res * A;
+// }
+// Console.WriteLine(res);
+
+
+// 2). Метод с циклом for
+
+int GetPow (int A, int B)
+{
+    int res =1 ;
+    for (int i = 0; i < B; i ++)
+    {
+        res = res * A;
+    }
+    return res;
+}
+int result = GetPow (3,5);
+Console.WriteLine($" A в степени B равно {result}");
