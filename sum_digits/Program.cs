637434aeca024387ca44197007b9fbc30e5c2c8e
Julia Machina <machinajg@gmail.com>
2023-02-09 17:17:57 +0300

    Sum of Digits project added

---
+++ sum_digits/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// 1). Цикл for
+
+// Console.Write("Введите число А:  ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int res = 0;
+// for (int i = 0; A > 0; i++)
+// {
+//     int B = A % 10;
+//     A = A / 10;
+//     res = res + B;
+// }
+// Console.WriteLine(res);
+
+
+// 2).  Метод
+
+int SumUp (int A)
+{
+    int res = 0;
+    for (int i = 0; A > 0; i++)
+    {
+        int B = A % 10;
+        A = A / 10;
+        res = res + B;
+    }
+    return res;
+}
+int GetSum = SumUp(9012);
+Console.WriteLine(GetSum);
